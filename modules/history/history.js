f6b961d0dbe83ceeecff7de59849b39f6f68a50a
blthiewes <blthiewes@gmail.com>
2016-06-04 22:47:47 -0500

    init

---
+++ modules/history/history.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+        
+    angular.module('myapp').controller('historyController', ['$scope', function($scope) {
+        $scope.greeting = 'Hola!';
+        
+        $scope.championships = [
+            {"year":2013, "owner":'Matt Neu'},
+            {"year":2014, "owner":'Matt Neu'},
+            {"year":2015, "owner":'Kyle Beck'}
+        ];
+    }]);
+})();
